b447975c1d323c52e6d071f802fe0470115fb69f
SafeDown <132012360+SafeDown@users.noreply.github.com>
2025-04-08 21:03:36 -0300

    Update q3.py by Wagner

--- respostas/q3.py
+++ respostas/q3.py
@@ -1,1 +1,3 @@
-#Crie um programa que exiba as seguintes informações, cada uma em uma linha:
+print("Nome:WagneRuan")
+print("Curso:Lincenciatura em Computação")
+print("Unidade de Ensino: IFPI - Zona Sul")#Crie um programa que exiba as seguintes informações, cada uma em uma linha:
